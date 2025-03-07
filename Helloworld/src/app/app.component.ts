8d385c2dd86ea21e84cd76cb8343637b4807e254
palakgu <palakjv304@gmail.com>
2025-03-07 11:28:02 +0530

    [Palak] add. To display “Hello from BridgeLabz” using One Way Data Bindings

---
+++ Helloworld/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Helloworld';
+  imgUrl = "./assets/BL_logo_square_png.png";
+
+ngOnInit() : void {
+  this.title = " Hello  From Bridgelabz ";
+}
+}
